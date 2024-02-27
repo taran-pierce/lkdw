0f78b4c2ef8204c62fe2f6c56aa1c1f4b26de7b4
Taran Pierce <taran.tpdesign@gmail.com>
2024-02-26 17:15:04 -0700

    main
    test build

--- frontend/declaration.d.ts
+++ frontend/declaration.d.ts
@@ -3,3 +3,10 @@ declare module '*.scss' {
   
   export default content;
 };
+
+declare module '@keystone-6/auth';
+declare module '@keystone-6/core';
+declare module '@keystone-6/cloudinary';
+declare module '@keystone-6/fields-document';
+declare module 'graphql';
+
