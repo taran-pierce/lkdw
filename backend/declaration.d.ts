a667a34c188c94dc67942a45258ebd9aebfc1ac2
Taran Pierce <taran.tpdesign@gmail.com>
2024-02-26 17:12:57 -0700

    main
    test build again

---
+++ backend/declaration.d.ts
@@ -0,0 +1,5 @@
+declare module '@keystone-6/auth';
+declare module '@keystone-6/core';
+declare module '@keystone-6/cloudinary';
+declare module '@keystone-6/fields-document';
+declare module 'graphql';
